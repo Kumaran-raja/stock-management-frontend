42c40051feb3aa898014db674912cdd4f7b6ecc2
Kumaranraja <team404errorfound@gmail.com>
2025-05-03 12:22:55 +0530

    cors

--- src/components/Entrypage.jsx
+++ src/components/Entrypage.jsx
@@ -63,7 +63,7 @@ function Entrypage() {
     try {
       const response = await fetch('https://stock-50026128252.development.catalystappsail.in/api/stock/submit', {
         method: 'POST',
-        mode: 'no-cors',
+        mode: 'cors',
         headers: {
           'Content-Type': 'application/json',
           'Accept': 'application/json'
