771b0ec9312c5d799f3d9150d8ab9f9694aa6245
Kumaranraja <team404errorfound@gmail.com>
2025-05-03 12:09:10 +0530

    submit code

--- src/components/Entrypage.jsx
+++ src/components/Entrypage.jsx
@@ -65,7 +65,8 @@ function Entrypage() {
         method: 'POST',
         mode: 'no-cors',
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
         },
         body: JSON.stringify({
           bagCode: formData.bagCode,
